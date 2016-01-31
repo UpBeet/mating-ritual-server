7c242a82c1cced52aa1f1421742240e8623a37ed
Peter Gyory <ptg3662@rit.edu>
2016-01-30 22:19:23 -0500

    Add room handler

---
+++ api/rooms.js
@@ -0,0 +1,32 @@
+import R from 'ramda';
+
+const rooms = {};
+
+export default {
+  createRoom: (roomKey) => {
+    if (rooms[roomKey]) throw new Error('Room already exists');
+    rooms[roomKey] = {};
+    return rooms;
+  },
+  joinRoom: (roomKey, userKey, socket) => {
+    rooms[roomKey][userKey] = socket;
+    return rooms;
+  },
+  leaveRoom: (roomKey, userKey) => {
+    delete rooms[roomKey][userKey];
+    return rooms;
+  },
+  deleteRoom: (roomKey) => {
+    delete rooms[roomKey];
+    return rooms;
+  },
+  send: (msg, ws) => {
+    ws.send(msg);
+    return msg;
+  },
+  sendRoom: (msg, roomKey) => {
+    const sendMsg = R.curry(this.send, msg);
+    R.map(sendMsg, rooms[roomKey]);
+    return rooms;
+  },
+};
