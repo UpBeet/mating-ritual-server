b86b0ca77a672de872649b55dd5cb9b00ea5161c
brax <baf9201@rit.edu>
2016-01-31 01:10:38 -0500

    PETER ROLLED THE DICE

--- api/rooms.js
+++ api/rooms.js
@@ -41,7 +41,7 @@ export const send = (action, data, ws) => {
 };
 
 export const sendRoom = (action, data, roomKey) => {
-  const sendMsg = R.curry(send, msg);
+  const sendMsg = R.curry(send, action, data);
   R.map(sendMsg, rooms[roomKey]);
   return rooms;
 };
