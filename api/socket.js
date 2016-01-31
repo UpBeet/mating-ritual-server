a3feba29e56ea813d2d81f77b9243e2080d702e9
brax <baf9201@rit.edu>
2016-01-31 01:50:12 -0500

    added more routes

--- api/socket.js
+++ api/socket.js
@@ -1,12 +1,24 @@
 import * as room from './rooms';
 
+const Emit = room.send;
+const EmitRoom = room.sendRoom;
+
 export default function (ws) {
   ws.on('message', (msg) => {
-    const event = JSON.parse(msg);
+    const { action, roomKey, data } = JSON.parse(msg);
 
-    switch(event.action){
-      case 'CREATE_ROOM': room.send('ROOM_KEY', room.createRoom(), ws); break;
-      case 'JOIN_ROOM': room.sendRoom('JOINED_ROOM', room.joinRoom(event.data, ws), event.data); break;
+    switch(action){
+      case 'CREATE_ROOM': Emit('ROOM_KEY', room.createRoom(), ws); break;
+
+      case 'JOIN_ROOM': EmitRoom('JOINED_ROOM', room.joinRoom(roomKey, ws), data); break;
+
+    //case 'BEGIN': EmitRoom('BEGIN', game.getJudge(roomKey), roomKey); break;
+
+      case 'SEND_PROMPT': EmitRoom('GET_PROMPT', data, roomKey); break;
+
+    //  case 'SEND_DANCE': EmitRoom('GET_DANCE', )
+
+      case 'PICK_WINNER': EmitRoom('ROUND_WINNER', data, roomKey); break;
     }
   });
 
