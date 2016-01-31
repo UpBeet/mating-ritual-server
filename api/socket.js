4a530cd4272e261e1a6347872006256f99b7a626
brax <baf9201@rit.edu>
2016-01-31 00:41:34 -0500

    added room to socket.js

--- api/socket.js
+++ api/socket.js
@@ -1,9 +1,9 @@
-//import room from './room';
+import room from './room';
 
 export default function (ws) {
   ws.on('message', (msg) => {
     const event = JSON.parse(msg);
-    
+
     switch(event.action){
       case 'CREATE_ROOM': room.sendRoom('ROOM_KEY', rooms.createRoom(), res);
       case 'JOIN_ROOM': rooms.sendRoom('JOINED_ROOM', rooms.joinRoom(event.data, ws), key);
